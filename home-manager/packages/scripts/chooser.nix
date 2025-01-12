4a3061e5cc13311bab2b89a70a6c777e8817c4e4
benjamin-voca <benjaminvocaa@gmail.com>
2025-01-12 23:03:03 +0100

    add wallpaper picker script

---
+++ home-manager/packages/scripts/chooser.nix
@@ -0,0 +1,17 @@
+{ pkgs, ... }:
+let
+  wppick = pkgs.writeShellScriptBin "wppick" ''paths=$(yazi --chooser-file=/dev/stdout ~/Pictures/Wallpapers | while read -r; do printf "%q " "$REPLY"; done)
+
+# Ensure there's no trailing space or newline in the path
+paths=$(echo "$paths" | xargs)
+
+if [[ -n "$paths" ]]; then
+  path="$paths"
+  echo "$path"
+  swww img "$path"
+fi
+'';
+in
+{
+  home.packages = [ wppick ];
+}
