dce297303fca13d770eb3d5376ca03db7d267b85
benjamin-voca <benjaminvocaa@gmail.com>
2025-03-19 11:18:59 +0100

    anyrun broken

--- home-manager/packages/scripts/swap_monitor.nix
+++ home-manager/packages/scripts/swap_monitor.nix
@@ -1,15 +1,13 @@
 
 { pkgs, ...}: 
-let swap_monitors = pkgs.writeShellScriptBin "swap_monitors" 
-
-''
+let swap_monitors = pkgs.writers.writePython3Bin "swap_monitors" {} ''
 import subprocess
 import json
 
 result = subprocess.run(['hyprctl', 'monitors', '-j'], capture_output=True, text=True)
-parsed_json = list(filter( lambda i: i["focused"]==False , json.loads(result.stdout)))  # noqa: E712
+parsed_json = list(filter( lambda i: i["focused"]==False , json.loads(result.stdout)))
 
-subprocess.run(['hyprctl', "dispatch", "focusmonitor", str(parsed_json[0]["id"])])
+subprocess.run(['hyprctl', 'dispatch', 'focusmonitor', str(parsed_json[0]['id'])])
 '';
 in {
   home.packages = [swap_monitors];
